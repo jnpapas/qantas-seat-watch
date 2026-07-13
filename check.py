783ce2debd3ba9f60bb4e280c6ab187434146add
James Papas <133964436+jnpapas@users.noreply.github.com>
2026-07-13 17:54:23 +1000

    Update check.py

--- check.py
+++ check.py
@@ -17,7 +17,7 @@ SEARCH_WINDOWS = [
     ("2027-07-01", "2027-07-31"),
 ]
 
-ALERT_PASSENGERS = 2       # trigger an email only when a fare bucket has >= this many seats
+ALERT_PASSENGERS = 1       # trigger an email only when a fare bucket has >= this many seats
 FETCH_PASSENGERS = 1       # query broadly so 1-seat results aren't filtered out server-side
 CABINS = "Business,First"
 DEST = ";EU"
